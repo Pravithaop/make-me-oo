1946b85b90ee5a6549949f862e53f11f96bb9d8d
PravithaOP <oppravitha@gmail.com>
2021-01-16 20:11:17 +0530

    Fix from value

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -9,15 +9,17 @@ public class Point {
         this.y = y;
     }
 
+    Point from = this;
+
     public double distance(Point to) {
-        Point from = this;
+
         double xDistance = getDistance(to.x, from.x);
         double yDistance = getDistance(to.y, from.y);
         return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
     }
 
     public double direction(Point to) {
-        Point from = this;
+
         double xDistance = getDistance(to.x, from.x);
         double yDistance = getDistance(to.y, from.y);
         return Math.atan2(yDistance, xDistance);
